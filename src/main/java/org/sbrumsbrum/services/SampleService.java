1767f961422fb9be27d5d5ebc892cfba42daf66b
Orazio Contarino <ocontarino@gmail.com>
2024-06-10 09:05:25 +0200

    init

---
+++ src/main/java/org/sbrumsbrum/services/SampleService.java
@@ -0,0 +1,5 @@
+package org.sbrumsbrum.services;
+
+public class SampleService {
+    public void sampleMethod(){}
+}
---
+++ src/test/java/org/sbrumsbrum/services/SampleServiceTest.java
@@ -0,0 +1,9 @@
+package org.sbrumsbrum.services;
+
+class SampleServiceTest {
+
+    void sampleMethod() {
+        SampleService sampleService = new SampleService();
+        sampleService.sampleMethod();
+    }
+}
